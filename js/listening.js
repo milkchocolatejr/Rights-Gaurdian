b790662d0e8f476e66ded17204c9146673b4e80e
tabletoprug <tabletoprug.og.dev@gmail.com>
2026-07-17 04:40:54 +0900

    Added notices and events

--- js/listening.js
+++ js/listening.js
@@ -1,7 +1,31 @@
+const releventEvents = [];
+
+class RelevantEvent {
+  constructor(eventData) {
+    this.metadata = {
+      seen: false,
+      postedTimestamp: new Date().toISOString()
+    };
+    this.event = eventData;
+  }
+}
+
 function toggleAnimation() {
-    var el = document.getElementById('listening-breathing');
-    console.debug("Breathing animation clicked, " + el.classList)
-    if (el) {
-        el.classList.toggle('paused');
-    }
+  var el = document.getElementById('listening-breathing');
+  if (el) {
+    el.classList.toggle('paused');
+  }
+}
+
+function pushReleventEvent(event) {
+  releventEvents.push(event);
+  var panel = document.getElementById('notification-panel');
+  if (panel) {
+    panel.insertAdjacentHTML('afterbegin',
+      '<div id="notification-' + releventEvents.length + '" class="notification-item">' +
+        '<h3 class="notification-title">' + event.event.title + '</h3>' +
+        '<p class="notification-text">' + event.event.noteText + '</p>' +
+      '</div>'
+    );
+  }
 }
